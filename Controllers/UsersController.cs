9286f4d264eebf8f3ce497b07f79c32cff4ed5a7
kavinda <kavinda@inexisconsulting.com>
2024-10-18 15:56:45 +0530

    Add API Get Enpoint and put Async

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using API.Data;
+using API.Entites;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private DataContext _dbContext;
+        public UsersController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _dbContext.User.ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}")]
+
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _dbContext.User.FindAsync(id);
+
+            if (user == null) return NotFound();
+
+            return Ok(user);
+        }
+    }
+}
