bf8e0393fa766219d154bcaf906757a70a271ff4
kavinda <kavinda@inexisconsulting.com>
2024-11-18 01:36:15 +0530

    Changes Program.cs file

--- Program.cs
+++ Program.cs
@@ -1,54 +1,3 @@
-//using API.Data;
-//using API.Extentions;
-//using API.Interface;
-//using API.Middleware;
-//using API.Services;
-//using Microsoft.AspNetCore.Authentication.JwtBearer;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.IdentityModel.Tokens;
-//using System.Text;
-
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-
-//builder.Services.AddApplicationServices(builder.Configuration);
-
-//builder.Services.AddIdentityServices(builder.Configuration);
-////builder.Services.AddCors();
-//builder.Services.AddDbContext<DataContext>(opt =>
-//{
-//    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-//});
-
-//var app = builder.Build();
-////app.UseMiddleware<ExceptionMiddleware>();
-////app.UseMiddleware<ExceptionMiddleware>();
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-////app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200/", "https://localhost:4200/"));
-////var app = builder.Build();
-//app.UseRouting();
-
-//app.UseMiddleware<ExceptionMiddleware>();
-
-//app.UseCors();
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthentication();
-//app.UseAuthorization();
-
-
-//app.MapControllers();
-
-//app.Run();
 using API.Data;
 using API.Extentions;
 using API.Interface;
@@ -59,41 +8,92 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddApplicationServices(builder.Configuration);
+
 builder.Services.AddIdentityServices(builder.Configuration);
+//builder.Services.AddCors();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-// Build the app
 var app = builder.Build();
-
-// Use exception middleware before other middleware
 //app.UseMiddleware<ExceptionMiddleware>();
-
-// Swagger for development
+//app.UseMiddleware<ExceptionMiddleware>();
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Configure other middlewares
-app.UseMiddleware<ExceptionMiddleware>();
+//app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200/", "https://localhost:4200/"));
+//var app = builder.Build();
 app.UseRouting();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200/", "https://localhost:4200/"));
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
+
 app.MapControllers();
 
 app.Run();
+//using API.Data;
+//using API.Extentions;
+//using API.Interface;
+//using API.Middleware;
+//using API.Services;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.IdentityModel.Tokens;
+//using System.Text;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddApplicationServices(builder.Configuration);
+//builder.Services.AddIdentityServices(builder.Configuration);
+//builder.Services.AddDbContext<DataContext>(opt =>
+//{
+//    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+//});
+
+//// Build the app
+//var app = builder.Build();
+
+//// Use exception middleware before other middleware
+////app.UseMiddleware<ExceptionMiddleware>();
+
+//// Swagger for development
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//// Configure other middlewares
+//app.UseMiddleware<ExceptionMiddleware>();
+//app.UseRouting();
+
+//app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200/", "https://localhost:4200/"));
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
 
