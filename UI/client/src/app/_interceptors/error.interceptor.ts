d992464e4b8872b3253a58b2c25bebe18e6682eb
kavinda <kavinda@inexisconsulting.com>
2024-11-22 09:03:35 +0530

    Update

---
+++ UI/client/src/app/_interceptors/error.interceptor.ts
@@ -0,0 +1,189 @@
+// import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { NavigationExtras, Router } from '@angular/router';
+// import { ToastrService } from 'ngx-toastr';
+// import { catchError, Observable } from 'rxjs';
+
+// export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+
+//   return next(req);
+// };
+
+// @Injectable()
+
+// export class ErrorInterceptor implements HttpInterceptor{
+
+//   constructor(private router: Router, private toastr : ToastrService) { }
+
+//   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+//     return next.handle(request).pipe(
+//       catchError((error: HttpErrorResponse)=>{
+//         if(error){
+//           switch(error.status){
+//             case 400:
+//               if(error.error.errors){
+//                 const modelStateErrors=[];
+//                 for(const key in error.error.errors){
+//                   if(error.error.errors[key]){
+//                     modelStateErrors.push(error.error.errors[key])
+//                   }
+//                 }
+//                 throw modelStateErrors;
+//               }else{
+//                 this.toastr.error(error.error, error.status.toString())
+//               }
+//             case 401:
+//               this.toastr.error('Unauthorised',error.status.toString())
+//               break;
+//             case 404:
+//               this.router.navigateByUrl('/not-found');
+//               break;
+//             case 500:
+//               const navigationExtras : NavigationExtras ={state :{error:error.error}};
+//               this.router.navigateByUrl('/server-error', navigationExtras)
+//               break;
+//             default:
+//               this.toastr.error('Something unexpected went wrong')
+//               console.log(error)
+//               break;
+//           }
+
+//         }
+//         throw error;
+//       })
+
+      
+
+//     )
+//   }
+  
+ 
+  
+// }
+
+// export class ErrorInterceptor implements HttpInterceptor {
+//   constructor(private router: Router, private toastr: ToastrService) {}
+
+//   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+//     return next.handle(request).pipe(
+//       catchError((error: HttpErrorResponse) => {
+//         if (error) {
+//           switch (error.status) {
+//             case 400:
+//               if (error.error.errors) {
+//                 const modelStateErrors = [];
+//                 for (const key in error.error.errors) {
+//                   if (error.error.errors[key]) {
+//                     modelStateErrors.push(error.error.errors[key]);
+//                   }
+//                 }
+//                 throw modelStateErrors; // Ends execution for case 400
+//               } else {
+//                 this.toastr.error(error.error, error.status.toString());
+//               }
+//               break; // Prevent fallthrough
+
+//             case 401:
+//               this.toastr.error('Unauthorised', error.status.toString());
+//               break;
+
+//             case 404:
+//               this.router.navigateByUrl('/not-found');
+//               break;
+
+//             case 500:
+//               const navigationExtras: NavigationExtras = { state: { error: error.error } };
+//               this.router.navigateByUrl('/server-error', navigationExtras);
+//               break;
+
+//             default:
+//               this.toastr.error('Something unexpected went wrong');
+//               console.log(error);
+//               break;
+//           }
+//         }
+
+//         throw error;
+//       })
+//     );
+//   }
+// }
+
+// import { HttpInterceptorFn } from '@angular/common/http';
+// import { ToastrService } from 'ngx-toastr';
+// import { inject } from '@angular/core';
+// import { catchError } from 'rxjs/operators';
+// import { throwError } from 'rxjs';
+
+// export const ErrorInterceptor: HttpInterceptorFn = (req, next) => {
+//   const toastr = inject(ToastrService); // Use Angular's DI system to inject services
+
+//   return next(req).pipe(
+//     catchError((error) => {
+//       if (error.status === 0) {
+//         // Network error
+//         toastr.error('Network error occurred', 'Error');
+//       } else {
+//         // HTTP error
+//         toastr.error(`Error ${error.status}: ${error.message}`, 'Server Error');
+//       }
+//       return throwError(() => error);
+//     })
+//   );
+// };
+
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router, NavigationExtras } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router, private toastr: ToastrService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error) {
+          switch (error.status) {
+            case 400:
+              if (error.error?.errors) {
+                const modelStateErrors: string[] = [];
+                for (const key in error.error.errors) {
+                  if (error.error.errors[key]) {
+                    modelStateErrors.push(error.error.errors[key]);
+                  }
+                }
+                modelStateErrors.forEach((err) => this.toastr.error(err, 'Validation Error'));
+              } else {
+                this.toastr.error(error.error, error.status.toString());
+              }
+              break;
+
+            case 401:
+              this.toastr.error('Unauthorised', error.status.toString());
+              break;
+
+            case 404:
+              this.router.navigateByUrl('/not-found');
+              break;
+
+            case 500:
+              const navigationExtras: NavigationExtras = { state: { error: error.error } };
+              this.router.navigateByUrl('/server-error', navigationExtras);
+              break;
+
+            default:
+              this.toastr.error('Something unexpected went wrong');
+              console.error(error);
+              break;
+          }
+        }
+
+        return throwError(() => error);
+      })
+    );
+  }
+}
--- UI/client/src/app/errors/test-error/test-error.component.ts
+++ UI/client/src/app/errors/test-error/test-error.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,5 +8,47 @@ import { Component } from '@angular/core';
   styleUrl: './test-error.component.css'
 })
 export class TestErrorComponent {
+  
+  constructor(private  http: HttpClient) {
+    
+    
+  }
+ baseurl = 'https://localhost:7237/api/'
 
+get404Error(){
+  this.http.get(this.baseurl+'buggy/not-found').subscribe({
+    next : response => console.log(response),
+    error: error=> console.log(error)
+    
+  })
+}
+
+get400Error(){
+  this.http.get(this.baseurl+'buggy/bad-request').subscribe({
+    next : response => console.log(response),
+    error: error=> console.log(error)
+    
+  })
+}
+get500Error(){
+  this.http.get(this.baseurl+'buggy/server-error').subscribe({
+    next : response => console.log(response),
+    error: error=> console.log(error)
+    
+  })
+}
+get401Error(){
+  this.http.get(this.baseurl+'buggy/auth').subscribe({
+    next : response => console.log(response),
+    error: error=> console.log(error)
+    
+  })
+}
+get400ValidationError(){
+  this.http.get(this.baseurl+'account/register').subscribe({
+    next : response => console.log(response),
+    error: error=> console.log(error)
+    
+  })
+}
 }
